7b525aae5cc16eedb96df2a0d94b50c3ec10ab8a
Vishal Deep Kanojia <vishal.deep@live.com>
2021-12-08 22:59:38 +0530

    Changed index.js to index.jsx

---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import Layout from "../components/Layout";
+import TextEditor from "../components/TextEditor";
+
+const IndexPage = () => {
+  return (
+    <Layout>
+      <TextEditor />
+    </Layout>
+  );
+};
+
+export default IndexPage;
