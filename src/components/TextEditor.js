c5e1043add8e892827bd2bb9b405ef817f802d98
Vishal Deep Kanojia <vishal.deep@live.com>
2021-12-08 23:00:02 +0530

    Added TextEditor component

---
+++ src/components/TextEditor.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const TextEditor = styled.textarea`
+  padding: 0.25rem;
+  width: 100%;
+  height: 100%;
+  border: none;
+  outline: none;
+  font-family: "Source Code Pro";
+`;
+
+export default TextEditor;
